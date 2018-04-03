3e4f79409a57e3f3ef1634e87872123c8b0b4ced
Roberto Albino <junior_wash@hotmail.com>
2018-04-02 23:54:34 -0300

    Criando modal de cadastro de usuários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
 
+import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-cadastro-usuario',
+  templateUrl: './cadastro-usuario/cadastro-usuario.component.html',
+  styleUrls: ['./cadastro-usuario/cadastro-usuario.component.css']
+})
+export class CadastroUsuarioComponent {
+  constructor(public activeModal: NgbActiveModal) { }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent { 
+
+  constructor(private modalService: NgbModal) {}
+  
+  open() {
+    this.modalService.open(CadastroUsuarioComponent);
+  }
+}
