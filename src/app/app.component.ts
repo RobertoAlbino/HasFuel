000cb2e4d9696fc5a866f2db4b7129f3128eb8de
Roberto Albino Junior <roberto.junior@sistemainfo.corp>
2018-04-04 14:41:12 -0300

    Atualizando a versão do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,6 @@ import { Component } from '@angular/core';
 import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-cadastro-usuario',
-  templateUrl: './cadastro-usuario/cadastro-usuario.component.html',
-  styleUrls: ['./cadastro-usuario/cadastro-usuario.component.css']
-})
-export class CadastroUsuarioComponent {
-  constructor(public activeModal: NgbActiveModal) { }
-}
-
-@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -21,6 +12,6 @@ export class AppComponent {
   constructor(private modalService: NgbModal) {}
   
   open() {
-    this.modalService.open(CadastroUsuarioComponent);
+    this.modalService.open(null);
   }
 }
